e09bd121afa558d548c724ce4f1696cbb06fab6b
MohammadShahheydar <m.shahheydar110@gmail.com>
2023-12-01 14:27:12 +0330

    complete header

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -10,17 +10,35 @@ import {Collapse, Typography} from "@mui/material";
 import IconMenu from "../icon/IconMenu";
 function Layout() {
     const [open, setOpen] = useState(false);
+
     return (
         <StyledBox width={'100%'} height={'100dvh'} backgroundColor={theme => theme.palette.background.paper}>
-            <StyledCard backgroundColor={theme => theme.palette.primary.main} p={10} borderRadius={10} width={'100%'}>
+            <StyledCard backgroundColor={theme => theme.palette.primary.main} p={10} borderRadius={10} width={'100%'} boxShadow={theme => theme.shadows[3]}>
                 <StyledStack flexDirection={'row'} width={'100%'} justifyContent={'flex-start'} alignItems={'center'}>
                     <StyledIconButton onClick={() => setOpen(prevState => !prevState)}>
                         <IconMenu color={theme => theme.palette.background.paper}/>
                     </StyledIconButton>
                 </StyledStack>
                 <Collapse in={open}>
-                    <StyledBox py={2} px={1}>
-                        <StyledStack gap={5} width={'100%'} justifyContent={'flex-start'}>
+                    <StyledBox py={2} px={1} position={'relative'}>
+                        <img
+                            style={{
+                                position: 'absolute',
+                                height: '90%',
+                                left: '0',
+                                opacity: .2,
+                            }}
+                            src={'/res/svg/dollar.svg'}
+                        />
+                        {/*<StyledBox*/}
+                        {/*    position={'absolute'}*/}
+                        {/*    zIndex={1}*/}
+                        {/*    width={'100%'}*/}
+                        {/*    height={'100%'}*/}
+                        {/*    // @ts-ignore*/}
+                        {/*    */}
+                        {/*/>*/}
+                        <StyledStack gap={5} width={'100%'} justifyContent={'flex-start'} position={'relative'} zIndex={2}>
                             <StyledStack>
                                 <Typography variant={'caption'} color={theme => theme.palette.primary.contrastText}>
                                     خوش آمدید
